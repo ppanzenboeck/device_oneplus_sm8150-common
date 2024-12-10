a93a7c798a06157e24a8db0ba74e14ed1cb73ce3
ppanzenboeck <panz.development@gmail.com>
2024-12-10 13:59:38 +0100

    RemovePackages: Google apps debloat

--- common.mk
+++ common.mk
@@ -10,6 +10,10 @@ DERP_VERSION_APPEND_TIME_OF_DAY=true
 EXTRA_UDFPS_ICONS=true
 TARGET_NOT_USES_BLUR=true
 
+# Remove Packages
+PRODUCT_PACKAGES += \
+    RemovePackages
+
 # Add common definitions for Qualcomm
 $(call inherit-product, hardware/qcom-caf/common/common.mk)
 
