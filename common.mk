5e7a00e74d3ef1719f4756977ab58d4d02862945
ppanzenboeck <panz.development@gmail.com>
2026-04-07 17:01:15 +0200

    fix media preview
    Change-Id: I08408e5a7b5c4ea928c480c350385c016c626e34

--- common.mk
+++ common.mk
@@ -232,7 +232,7 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/com.android.nfc_extras.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.android.nfc_extras.xml \
     frameworks/native/data/etc/com.nxp.mifare.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.nxp.mifare.xml
 
-$(call soong_config_set,stagefright,target_disable_thumbnail_block_model,true)
+$(call soong_config_set_bool,stagefright,target_disable_thumbnail_block_model,true)
 
 # Overlays
 $(call inherit-product, hardware/oplus/overlay/generic/generic.mk)
